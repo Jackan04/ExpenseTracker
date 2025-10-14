7bf9e7d9a0c06f433757522934956d19c6ddfe02
Jackan04 <jacob.asker@icloud.com>
2025-10-14 13:26:31 +0200

    Updated: Expense.cs with additional properties Feature: added test in Expenses page for inserting new data to Firebase

--- ExpenseTracker/Models/Expense.cs
+++ ExpenseTracker/Models/Expense.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ExpenseTracker.Models;
 
 public class Expense
 {
+    [Required]
     public string Title { get; set; } = "";
+
+    [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than 0")]
     public decimal Amount { get; set; }
+
+    [Required]
     public string Category { get; set; } = "";
+
     public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
--- ExpenseTracker/obj/Debug/net9.0/ExpenseTracker.AssemblyInfo.cs
+++ ExpenseTracker/obj/Debug/net9.0/ExpenseTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ddfb85d0d1232c8602f2c2dcbc991cbf9612bc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76f253cd5684b1792d33626674a605e00bbe84a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
